eef541683fbf88a9f4c74ef9b809c14824d8549b
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-29 20:44:41 +0700

    Update main.py

--- trading_app/receiver/src/main.py
+++ trading_app/receiver/src/main.py
@@ -28,7 +28,7 @@ import uvloop
 import redis.asyncio as aioredis
 
 # Application imports
-from trading_app.configuration import config, config_oci
+from configuration import config, config_oci
 from trading_app.restful_api.deribit import end_point_params_template
 from trading_app.receiver import deribit_ws as receiver_deribit
 from trading_app.receiver import distributing_ws_data as distr_deribit
