9b7478abe08342be90e6fd3e69aa4a482a0cae57
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-29 08:41:21 +0700

    Update main.py

--- receiver/main.py
+++ receiver/main.py
@@ -9,7 +9,7 @@ import os
 from contextlib import AsyncExitStack
 
 # Fix module path
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from shared.config import load_config, get_env
 from shared.logging import setup_logging
