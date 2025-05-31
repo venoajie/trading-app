2b8266aa5e894af6c6b2f742f79d76e80d2b1b14
venoajie <59521823+venoajie@users.noreply.github.com>
2025-05-31 13:09:01 +0700

    .

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ async def trading_main() -> None:
     
     exchange = "deribit"
     sub_account_id = "deribit-148510"
-    config_file = "/app/config_strategies.toml"
+    config_file = "/app/configuration/config_strategies.toml"
 
     try:
         # Load configurations
