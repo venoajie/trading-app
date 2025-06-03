6fcb8703f0c7e104923d9e4781c22875add1bad6
venoajie <59521823+venoajie@users.noreply.github.com>
2025-06-04 05:51:44 +0700

    Update starter.py

--- trading/receiver/src/receiver/deribit/starter.py
+++ trading/receiver/src/receiver/deribit/starter.py
@@ -8,7 +8,7 @@ from loguru import logger as log
 
 # user defined formulas
 from shared.db import sqlite as db_mgt
-from restful_api.deribit import end_point_params_template as end_point
+from scripts.restful_api.deribit import end_point_params_template as end_point
 from shared.utils import (
     error_handling,
     pickling,
