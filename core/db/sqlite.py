21dc63c6d90a2318dae952479b5152ec2a9966e2
venoajie <59521823+venoajie@users.noreply.github.com>
2025-06-05 06:32:54 +0700

    .

--- core/db/sqlite.py
+++ core/db/sqlite.py
@@ -13,7 +13,7 @@ import aiosqlite
 from loguru import logger as log
 
 # user defined formulas
-from src.shared.db.redis import publishing_specific_purposes
+from core.db.redis import publishing_specific_purposes
 
 from src.shared.utils import (
     error_handling,
