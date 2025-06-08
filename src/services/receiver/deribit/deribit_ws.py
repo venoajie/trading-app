4fe67cac31ce6c79f5c9cd24a8258b47d6337e1b
venoajie <59521823+venoajie@users.noreply.github.com>
2025-06-08 20:19:05 +0700

    Update deribit_ws.py

--- src/services/receiver/deribit/deribit_ws.py
+++ src/services/receiver/deribit/deribit_ws.py
@@ -191,7 +191,7 @@ class StreamingAccountData:
                         await client_redis.xadd(
                             "stream:market_data",
                             {"data": orjson.dumps(message_dict["params"]).decode()},
-                            maxlen=(10000, True)
+                            maxlen=(10000)
                             )
                         
                     except Exception as e:
