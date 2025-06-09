a5a79b7f8a96f7e85314c1325a66759b3743cd76
venoajie <59521823+venoajie@users.noreply.github.com>
2025-06-09 13:46:21 +0700

    Update distributing_ws_data.py

--- src/services/distributor/deribit/distributing_ws_data.py
+++ src/services/distributor/deribit/distributing_ws_data.py
@@ -101,7 +101,7 @@ async def stream_consumer(
             pending = await redis.xpending_range(
                 STREAM_NAME,
                 GROUP_NAME,
-                idletime=30000  # 30 seconds
+                idle=30000  # 30 seconds
             )
             if pending:
                 await redis.xclaim(
